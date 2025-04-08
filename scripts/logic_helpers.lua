0e9b1c0c3c576df657cc36e0eda86fc5f690d702
cloudofdarkhole6 <bernd.schimek@gmail.com>
2025-04-08 12:32:09 +0200

    Test logic setting (didnt work)

--- scripts/logic_helpers.lua
+++ scripts/logic_helpers.lua
@@ -2,3 +2,10 @@ function can_improved_jump()
   return has("heli-pack") or has("thruster-pack")
 end
 
+function oozla_store_end()
+  local fpg = Tracker:FindObjectForCode("first_person_setting").CurrentStage
+  if fpg >= 2
+    return true
+  end
+  return has("dynamo")
+end
